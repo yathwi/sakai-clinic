448d2eefc41691a59e50bfd29f8a1fdfbfe04beb
yathwi <kaitoman.r.3@gmail.com>
2024-02-02 09:48:22 +0900

    header

--- app/inmode/layout.tsx
+++ app/inmode/layout.tsx
@@ -13,7 +13,7 @@ export default function InModeLayout({ children }: { children: React.ReactNode }
   return (
     <main className=" flex justify-center bg-red-50">
       <div className=" max-w-[414px]">
-        {/* <PageHeader /> */}
+        <PageHeader />
         <div>{children}</div>
         <Fotter />
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import styles from './page.module.css';
 import ButtonLink from '@/app/_components/ButtonLink';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Fotter } from './_components/Gummy-Smile/Fotter';
 
 export const revalidate = 60;
 
@@ -16,29 +17,34 @@ export default async function Page() {
   console.log(1, process.env.MICROCMS_API_KEY);
   console.log(111);
   return (
-    <div className=" bg-slate-50  w-full flex justify-center">
-      <div className=" text-center">
-        <div className=" py-40  ">
-          <p className="font-bold text-lg">ただいまサイトリニューアル中です</p>
-          <div className=" mt-5">
-            <p className="font-bold text-lg">準備済みコンテンツ</p>
-            <div className=" mt-10 text-center">
-              <Link href="/gummy-smile" legacyBehavior>
-                <a className=" py-5 text-white rounded-xl bg-green-500 hover:opacity-80  px-10">
-                  ガミースマイル
-                </a>
-              </Link>
-            </div>
-            <div className=" mt-10 text-center">
-              <Link href="/inmode" legacyBehavior>
-                <a className=" py-5 text-white rounded-xl bg-green-500 hover:opacity-80  px-10">
-                  インモード
-                </a>
-              </Link>
+    <div className=" h-screen w-full bg-slate-50 ">
+      <div className="flex justify-center">
+        <div className=" text-center">
+          <div className=" py-40  ">
+            <p className="font-bold text-lg">ただいまサイトリニューアル中です</p>
+            <div className=" mt-5">
+              <p className="font-bold text-lg">準備済みコンテンツ</p>
+              <div className=" mt-10 text-center">
+                <Link href="/gummy-smile" legacyBehavior>
+                  <a className=" py-5 text-white rounded-xl bg-green-500 hover:opacity-80  px-10">
+                    ガミースマイル
+                  </a>
+                </Link>
+              </div>
+              <div className=" mt-10 text-center">
+                <Link href="/inmode" legacyBehavior>
+                  <a className=" py-5 text-white rounded-xl bg-green-500 hover:opacity-80  px-10">
+                    インモード
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className=" absolute bottom-0 w-full">
+        <Fotter />
+      </div>{' '}
     </div>
   );
 }
